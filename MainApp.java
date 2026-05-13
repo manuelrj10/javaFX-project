c2a1038f9b58daefff25f70635aac81719b66ede
Manuel Rajesh <manuelrajesh39@gmail.com>
2026-05-13 13:59:04 +0530

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,14 @@
+package app;
+
+import javafx.application.Application;
+import ui.Home;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Application.launch(Home.class,args);
+	
+
+	}
+
+}
